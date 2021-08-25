3228253b88691f109eb609f445c12704e9c96788
yuan9 <yuan9@vt.edu>
2021-08-25 11:46:35 -0400

    Update testbench_rng.v

--- LED-TI/testbench_rng.v
+++ LED-TI/testbench_rng.v
@@ -1,3 +1,24 @@
+/***************************************************************************************************/
+/*
+ * Virginia Tech
+ * Secure Embedded Systems Lab
+ *
+ * Copyright (C) 2017 Virginia Tech
+ *
+ * Written in 2017 by Yuan Yao (yuan9@vt.edu), Mo Yang(ymo6@vt.edu)
+ *
+ * This software is distributed under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version. We are in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+ /**************************************************************************************************/
+
 module rng_test;
 
    reg clk;
@@ -27,7 +48,6 @@ module rng_test;
 	  vrst = 1;
 
 	#100 rd = 1'h1;
-	//#400 rd = 1'h0;
 	
 	#10000
 $stop;
